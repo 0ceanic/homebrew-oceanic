cda777d83635f6527ade6ce11686dc32b47d5139
0ceanic <gv@ocenic.io>
2019-04-09 00:17:21 -0400

    fixing sha256 on formula

--- Formula/lgtpx-backup.rb
+++ Formula/lgtpx-backup.rb
@@ -2,7 +2,7 @@ class LgtpxBackup < Formula
   desc "Command-line interface for backing up the LogTen Pro X database"
   homepage "https://github.com/0ceanic/lgtpx-backup"
   url "https://github.com/0ceanic/lgtpx-backup/archive/0.0.1.tar.gz"
-  sha256 "db83a1ebcbcb86c34970385182f68b272d839a107f23b8f7a2eef841738da72b"
+  sha256 "8e602455f5992d07560be947aa299c0de6a5332f987332322b237cacdce3e80f"
   head "https://github.com/0ceanic/lgtpx-backup.git"
 
   bottle :unneeded
