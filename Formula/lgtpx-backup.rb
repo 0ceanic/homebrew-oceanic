080fba75618759c8a17713335c1beda2f9c69f22
0ceanic <gv@ocenic.io>
2019-04-08 23:55:54 -0400

    setting up forumla

--- Formula/lgtpx-backup.rb
+++ Formula/lgtpx-backup.rb
@@ -1,4 +1,4 @@
-class Lgtpx-backup < Formula
+class LgtpxBackup < Formula
   desc "Command-line interface for backing up the LogTen Pro X database"
   homepage "https://github.com/0ceanic/lgtpx-backup"
   url "https://github.com/0ceanic/lgtpx-backup/archive/0.0.1.tar.gz"
