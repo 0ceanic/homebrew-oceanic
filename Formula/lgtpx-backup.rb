4d99f0b9a0665f36c3debdf188e41eae08828d07
0ceanic <0ceanic@users.noreply.github.com>
2019-10-31 21:21:14 -0400

    bumping initial release to 0.1.0

--- Formula/lgtpx-backup.rb
+++ Formula/lgtpx-backup.rb
@@ -1,8 +1,8 @@
 class LgtpxBackup < Formula
   desc "Command-line interface for backing up the LogTen Pro X database"
   homepage "https://github.com/0ceanic/lgtpx-backup"
-  url "https://github.com/0ceanic/lgtpx-backup/archive/0.0.1.tar.gz"
-  sha256 "8e602455f5992d07560be947aa299c0de6a5332f987332322b237cacdce3e80f"
+  url "https://github.com/0ceanic/lgtpx-backup/archive/0.1.0.tar.gz"
+  sha256 "b07856b0ad2c6f6c8739bd70ed4b8ad369418b0dc7a95bab9fcfcf5c91ab76ed"
   head "https://github.com/0ceanic/lgtpx-backup.git"
 
   bottle :unneeded
